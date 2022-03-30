f6ef562a1b6826c4a352370a4e79b8a139c43c64
Yaqoob <sualehfarooq65@gmail.com>
2022-03-30 09:15:33 +0500

    JS Table Assignment

---
+++ index.js
@@ -0,0 +1,44 @@
+let num = prompt("Please Enter Your Desired Table Number")
+
+document.write("<h1> Table of ", num, "</h1>")
+document.write("<br>")
+
+
+
+document.write( "<h2>", num, " x ", 1, " = ",
+    num * 1 , "</h2>" )
+
+document.write("<h2>", num, " x ", 2, " = ",
+    num * 2, "</h2>")
+
+
+document.write("<h2>", num, " x ", 3, " = ",
+    num * 3, "</h2>")
+
+
+document.write("<h2>", num, " x ", 4, " = ",
+    num * 4, "</h2>")
+
+
+document.write("<h2>", num, " x ", 5, " = ",
+    num * 5, "</h2>")
+
+
+document.write("<h2>", num, " x ", 6, " = ",
+    num * 6 ,"</h2>")
+
+
+document.write("<h2>", num, " x ", 7, " = ",
+    num * 8, "</h2>")
+
+
+document.write( "<h2>", num, " x ", 8, " = ",
+    num * 8, "</h2>")
+
+
+document.write("<h2>", num, " x ", 9, " = ",
+    num * 9, "</h2>")
+
+
+document.write("<h2>", num, " x ", 10, " = ",
+    num * 10, "</h2>")
